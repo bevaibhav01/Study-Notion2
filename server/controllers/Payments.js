9947afd8dbd24110ec0f50ef5799221810a4e05f
bevaibhav01 <vaibhavmahale55@gmail.com>
2023-11-07 22:36:59 +0530

    payment

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -6,4 +6,97 @@ const User=require('../models/User');
 
 const mailSender=require('../utils/mailSender');
 
-const {courseE}
+const {courseEnrollmentEmail}=require('../mail/templates/courseEnrollmentEmail');
+
+//capture the payment and initiate the razorpay order
+
+exports.capturePayment=async (req,res)=>{
+    try{
+
+        //get course id and usrer id
+        const {courseId}=req.body
+        const userId=req.user.id;
+        //validation 
+        if(!userId||!courseId){
+            return res.status(200).json({
+                success:false,
+                message:'details missing'
+            })
+        }
+        //valid course
+
+        const courseDetails=await Course.findById(courseId);
+        if(!courseDetails){
+            return res.status(200).json({
+                success:false,
+                message:'not a valid course'
+            }) 
+        }
+
+        //valid user
+
+        const userDetails=await User.findById(userId);
+        if(!userDetails){
+            return res.status(200).json({
+                success:false,
+                message:'not a valid user'
+            })  
+        }
+        //already buyed course or not
+        const uid=new mongoose.Types.ObjectId(userId);
+        
+        if(Course.studentsEnrolled.includes(uid)){
+            return res.status(200).json({
+                success:false,
+                message:'Already enrolled'
+            })  
+        }
+
+        //create order
+        const amount=Course.price;
+        const currency="INR";
+
+        const options={
+            amount:amount*100,
+            currency,
+            receipt:Math.random(Date.now()).toString(),
+            notes:{
+                courseId:courseId,
+                userId:userId,
+            }
+
+
+        }
+        try{
+
+            //initiate the payment
+            const paymentResponse=await instance.orders.create(options);
+            console.log(paymentResponse);
+            
+
+        }catch(error){
+            return res.status(200).json({
+                success:false,
+                message:'could not initiate payment'
+            })  
+        }
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            courseName:courseDetails.courseName,
+            courseDescription:courseDetails.courseDescription,
+            orderId:paymentResponse.id,
+            amount:paymentResponse.amount,
+        })
+
+    }catch(error){
+        return res.status(200).json({
+            success:false,
+            message:'payment went wrong'
+        })  
+    }
+}
+
+
+//verify signature
