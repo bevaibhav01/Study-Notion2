336579fc883b4ece59c7eaa4197cbca8b6f3844f
bevaibhav01 <vaibhavmahale55@gmail.com>
2023-11-07 22:00:37 +0530

    templates mail

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -1,1 +1,9 @@
-const {instance}
+const {instance}=require('../config/razorpay');
+
+const Course=require('../models/Course');
+
+const User=require('../models/User');
+
+const mailSender=require('../utils/mailSender');
+
+const {courseE}
