7e32182684ab6f49a2a705097a19d77684828bf0
bevaibhav01 <vaibhavmahale55@gmail.com>
2023-12-05 17:49:03 +0530

    frontend homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import {FaArrowRight} from 'react-icons/fa'
 import { HighlightText } from '../components/core/HomePage/HighlightText'
 import CTAButton from '../components/core/HomePage/Button'
 import Banner from '../assets/Images/banner.mp4'
+import CodeBlocks from '../components/core/HomePage/CodeBlocks'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
 
 
 
@@ -18,7 +20,7 @@ const Home = () => {
         <Link to={"/signup"}>
         <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
           transition-all duration-200 hover:scale-95 w-fit'>
-            <div className='flex flex-row items-center gap-rounded-full px-10 py-[5px] group-hover:bg-richblack-900'>
+            <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900'>
                 <p>Become a Instructor</p>
                 <FaArrowRight></FaArrowRight>
             </div>
@@ -41,8 +43,9 @@ const Home = () => {
 
         </div>
 
-        <div className='mx-3 my-12  shadow-blue-200 '>
+        <div className='mx-3 my-12 shadow-[10px_-5px_50px_-5px]  shadow-blue-200 '>
             <video
+               className='shadow-[20px_20px_rgba(255,255,255)]'
                 muted
                 loop
                 autoPlay
@@ -52,6 +55,94 @@ const Home = () => {
 
             </video>
         </div>
+
+        {/* code section 1 */}
+
+        <div>
+           <CodeBlocks 
+
+           position={"lg:flex-row"}
+
+           heading={
+            <div className='text-4xl font-semibold'>
+              Unlock Your <HighlightText
+              text={"coding potential "}
+              />
+              with our online course
+
+             
+            </div>
+           }
+
+           subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."}
+            
+           ctabtn1={
+            {
+              text:"try it yourself",
+              linkto:"/signup",
+              active:true,
+            }
+           }
+
+           ctabtn2={
+            {
+              text:"learn more",
+              linkto:"/login",
+              active:false,
+            }
+           }
+
+           codeColor={"text-yellow-25"}
+           codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+           backgroundGradient={<div className="codeblock1 absolute"></div>}
+
+
+            
+
+           
+           
+           >
+
+           </CodeBlocks>
+
+            {/* Code Section 2 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
+                Start
+                <HighlightText text={"coding in seconds"} />
+              </div>
+            }
+            subheading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              text: "Continue Lesson",
+              link: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              text: "Learn More",
+              link: "/signup",
+              active: false,
+            }}
+            codeColor={"text-white"}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            backgroundGradient={<div className="codeblock2 absolute"></div>}
+          />
+        </div>
+
+
+
+        </div>
+
+        <ExploreMore/>
+
+        
+
+
         
 
 
