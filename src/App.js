6039f4071fa831cf376162fc6c0e9a59e044fd4b
bevaibhav01 <vaibhavmahale55@gmail.com>
2024-01-01 00:34:08 +0530

    dashboard for students

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from '../src/pages/About'
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
 
 
 function App() {
@@ -65,14 +69,44 @@ function App() {
       
       */}
 
-    <Route
-          path="/about"
-          element={
-            
-              <About />
-            
-          }
-        />
+   <Route 
+      element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      }
+    >
+      <Route path="dashboard/my-profile" element={<MyProfile />} />
+      
+      {/* <Route path="dashboard/Settings" element={<Settings />} /> */}
+      
+
+      {/* {
+        user?.accountType === ACCOUNT_TYPE.STUDENT && (
+          <>
+          <Route path="dashboard/cart" element={<Cart />} />
+          <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+          </>
+        )
+      }
+
+      {
+        user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+          <>
+          <Route path="dashboard/instructor" element={<Instructor />} />
+          <Route path="dashboard/add-course" element={<AddCourse />} />
+          <Route path="dashboard/my-courses" element={<MyCourses />} />
+          <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
+          
+          </>
+        )
+      } */}
+
+
+    </Route>
+
+    <Route path="*" element={<Error />} />
+        
     </Routes>
 
    
